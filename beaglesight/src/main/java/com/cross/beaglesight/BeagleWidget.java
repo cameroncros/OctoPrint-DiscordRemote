c8fff02a5b89a1a6c9dd67674a02134d44974e7a
cameron <cameroncros@gmail.com>
2014-12-20 15:25:13 +1100

    Added widget to beaglesight

---
+++ beaglesight/src/main/java/com/cross/beaglesight/BeagleWidget.java
@@ -0,0 +1,126 @@
+package com.cross.beaglesight;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link BeagleWidgetConfigureActivity BeagleWidgetConfigureActivity}
+ */
+public class BeagleWidget extends AppWidgetProvider {
+
+    private static final String PlusCoarse = "PlusCoarse";
+    private static final String PlusFine = "PlusFine";
+    private static final String MinusCoarse = "MinusCoarse";
+    private static final String MinusFine = "MinusFine";
+    private static final int Fine = 1;
+    private static final int Coarse = 5;
+
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            BeagleWidgetConfigureActivity.deleteTitlePref(context, appWidgetIds[i]);
+            BeagleWidgetConfigureActivity.deleteDistPref(context, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int appWidgetId = intent.getIntExtra("widgetId", 0);
+        int dist=20;
+        //super.onReceive(context, intent);
+        switch (intent.getAction()) {
+            case PlusCoarse:
+                dist = BeagleWidgetConfigureActivity.loadDistancePref(context, appWidgetId);
+                dist += Coarse;
+                BeagleWidgetConfigureActivity.saveDistancePref(context, appWidgetId, dist);
+                break;
+            case PlusFine:
+                dist = BeagleWidgetConfigureActivity.loadDistancePref(context, appWidgetId);
+                dist += Fine;
+                BeagleWidgetConfigureActivity.saveDistancePref(context, appWidgetId, dist);
+                break;
+            case MinusCoarse:
+                dist = BeagleWidgetConfigureActivity.loadDistancePref(context, appWidgetId);
+                dist -= Coarse;
+                BeagleWidgetConfigureActivity.saveDistancePref(context, appWidgetId, dist);
+                break;
+            case MinusFine:
+                dist = BeagleWidgetConfigureActivity.loadDistancePref(context, appWidgetId);
+                dist -= Fine;
+                BeagleWidgetConfigureActivity.saveDistancePref(context, appWidgetId, dist);
+                break;
+        }
+
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+
+        updateAppWidget(context, appWidgetManager, appWidgetId);
+
+
+    }
+
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.beagle_widget);
+
+        views.setOnClickPendingIntent(R.id.imageButtonPlusCoarse,  getPendingSelfIntent(context, PlusCoarse,  appWidgetId));
+        views.setOnClickPendingIntent(R.id.imageButtonPlusFine,    getPendingSelfIntent(context, PlusFine,    appWidgetId));
+        views.setOnClickPendingIntent(R.id.imageButtonMinusCoarse, getPendingSelfIntent(context, MinusCoarse, appWidgetId));
+        views.setOnClickPendingIntent(R.id.imageButtonMinusFine,   getPendingSelfIntent(context, MinusFine,   appWidgetId));
+
+        String bowname = BeagleWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+        int dist = BeagleWidgetConfigureActivity.loadDistancePref(context, appWidgetId);
+
+        BowManager bm = BowManager.getInstance(null);
+        PositionCalculator pc = bm.getPositionCalculator(bowname);
+
+        double pos = pc.calcPosition(dist);
+
+        views.setTextViewText(R.id.widgetDistance, PositionCalculator.getDisplayValue(dist,0));
+        views.setTextViewText(R.id.widgetPosition, PositionCalculator.getDisplayValue(pos, 2));
+        views.setTextViewText(R.id.widgetBowName, bowname);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(new ComponentName(context, BeagleWidget.class), views);
+    }
+
+    protected static PendingIntent getPendingSelfIntent(Context context, String action, int appWidgetId) {
+        Intent intent = new Intent(context, BeagleWidget.class);
+        intent.setAction(action);
+        intent.putExtra("widgetId", appWidgetId);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+}
+
+
---
+++ beaglesight/src/main/java/com/cross/beaglesight/BeagleWidgetConfigureActivity.java
@@ -0,0 +1,132 @@
+package com.cross.beaglesight;
+
+import android.app.Activity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+
+/**
+ * The configuration screen for the {@link BeagleWidget BeagleWidget} AppWidget.
+ */
+public class BeagleWidgetConfigureActivity extends Activity {
+
+    int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    private static final String PREFS_NAME = "com.cross.beaglesight.BeagleWidget";
+    private static final String PREF_PREFIX_NAME = "appwidgetname_";
+    private static final String PREF_PREFIX_DIST = "appwidgetdist_";
+
+    public BeagleWidgetConfigureActivity() {
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        // Set the result to CANCELED.  This will cause the widget host to cancel
+        // out of the widget placement if the user presses the back button.
+        setResult(RESULT_CANCELED);
+
+        setContentView(R.layout.beagle_widget_configure);
+        findViewById(R.id.add_button).setOnClickListener(mOnClickListener);
+
+        // Find the widget id from the intent.
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        // If this activity was started with an intent without an app widget ID, finish with an error.
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+            return;
+        }
+
+        Spinner spinner = (Spinner) findViewById(R.id.bowListSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        BowManager bm = BowManager.getInstance(this);
+        ArrayList<String> bows = new ArrayList<String>(bm.getBowList());
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, bows);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            final Context context = BeagleWidgetConfigureActivity.this;
+
+            // When the button is clicked, store the string locally
+            Spinner mySpinner=(Spinner) findViewById(R.id.bowListSpinner);
+            String choice = mySpinner.getSelectedItem().toString();
+            saveTitlePref(context, mAppWidgetId, choice);
+            saveDistancePref(context, mAppWidgetId, 20);
+            // It is the responsibility of the configuration activity to update the app widget
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            BeagleWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId);
+
+            // Make sure we pass back the original appWidgetId
+            Intent resultValue = new Intent();
+            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+            setResult(RESULT_OK, resultValue);
+            finish();
+        }
+    };
+
+    // Write the prefix to the SharedPreferences object for this widget
+    static void saveTitlePref(Context context, int appWidgetId, String text) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putString(PREF_PREFIX_NAME + appWidgetId, text);
+        prefs.commit();
+    }
+
+    // Read the prefix from the SharedPreferences object for this widget.
+    // If there is no preference saved, get the default from a resource
+    static String loadTitlePref(Context context, int appWidgetId) {
+        return "Cameron G5"; //TODO:
+//        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+//        String titleValue = prefs.getString(PREF_PREFIX_NAME + appWidgetId, context.getString(R.string.app_name));
+//        return titleValue;
+
+    }
+
+    static void deleteTitlePref(Context context, int appWidgetId) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.remove(PREF_PREFIX_NAME + appWidgetId);
+        prefs.commit();
+    }
+
+    // Write the prefix to the SharedPreferences object for this widget
+    static void saveDistancePref(Context context, int appWidgetId, int dist) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.putInt(PREF_PREFIX_DIST + appWidgetId, dist);
+        prefs.commit();
+    }
+
+    // Read the prefix from the SharedPreferences object for this widget.
+    // If there is no preference saved, get the default from a resource
+    static int loadDistancePref(Context context, int appWidgetId) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, 0);
+        int titleValue = prefs.getInt(PREF_PREFIX_DIST + appWidgetId, 20);
+        return titleValue;
+
+    }
+
+    static void deleteDistPref(Context context, int appWidgetId) {
+        SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+        prefs.remove(PREF_PREFIX_DIST + appWidgetId);
+        prefs.commit();
+    }
+}
+
+
+
