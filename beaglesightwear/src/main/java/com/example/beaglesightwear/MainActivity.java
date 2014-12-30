5192c7d8bde3a40f06e425dcfb8e3378ca902126
cameron <cameroncros@gmail.com>
2014-12-30 17:04:12 +1100

    Updated google wear layouts, need file sync to progress further

--- beaglesightwear/src/main/java/com/example/beaglesightwear/MainActivity.java
+++ beaglesightwear/src/main/java/com/example/beaglesightwear/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.beaglesightwear;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.wearable.view.WatchViewStub;
+import android.support.wearable.view.GridViewPager;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -13,12 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+        final GridViewPager pager = (GridViewPager) findViewById(R.id.pager);
+        pager.setAdapter(new SampleGridPagerAdapter(this, getFragmentManager()));
     }
 }
---
+++ beaglesightwear/src/main/java/com/example/beaglesightwear/ShowBowFragment.java
@@ -0,0 +1,80 @@
+package com.example.beaglesightwear;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.cross.beaglesightlibs.BowManager;
+import com.cross.beaglesightlibs.PositionCalculator;
+
+/**
+ * Created by cameron on 30/12/14.
+ */
+
+public class ShowBowFragment extends Fragment implements View.OnClickListener {
+    View view = null;
+    String bowName = null;
+    double distance = 20;
+
+    private static final int Fine = 1;
+    private static final int Coarse = 5;
+
+    @Override
+    public void setArguments(Bundle args) {
+        bowName = (String)args.get("bowName");
+        if (view != null) {
+            TextView tv = (TextView)view.findViewById(R.id.bowName);
+            tv.setText(bowName);
+        }
+        super.setArguments(args);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.show_bow_fragment, container, false);
+        Button bt = (Button)view.findViewById(R.id.imageButtonMinusCoarse);
+        bt.setOnClickListener(this);
+        bt = (Button)view.findViewById(R.id.imageButtonPlusCoarse);
+        bt.setOnClickListener(this);
+        bt = (Button)view.findViewById(R.id.imageButtonMinusFine);
+        bt.setOnClickListener(this);
+        bt = (Button)view.findViewById(R.id.imageButtonPlusFine);
+        bt.setOnClickListener(this);
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageButtonMinusCoarse:
+                distance -= Coarse;
+                break;
+            case R.id.imageButtonMinusFine:
+                distance -= Fine;
+                break;
+            case R.id.imageButtonPlusCoarse:
+                distance += Coarse;
+                break;
+            case R.id.imageButtonPlusFine:
+                distance += Fine;
+                break;
+        }
+        BowManager bm = BowManager.getInstance(getActivity().getApplicationContext());
+        PositionCalculator pc = bm.getPositionCalculator(bowName);
+        double position = pc.calcPosition(distance);
+        TextView tv = (TextView)view.findViewById(R.id.widgetDistance);
+        tv.setText(Double.toString(distance));
+        tv = (TextView)view.findViewById(R.id.widgetPosition);
+        tv.setText(Double.toString(position));
+    }
+}
