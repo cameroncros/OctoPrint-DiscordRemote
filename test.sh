64b479fd3e5e522532fb375d59b90d6bbe3d9c1b
Cameron Cross <cameroncros@gmail.com>
2018-05-15 19:51:05 +1000

    DR-1: Fixed up test scripts to reset config every run

--- test.sh
+++ test.sh
@@ -6,6 +6,6 @@ python2 -m virtualenv testenv
 source testenv/bin/activate
 pip install https://get.octoprint.org/latest
 python2 setup.py develop
-cp -rf testconfig testenv/config
+cp -rf testconfig testenv/
 python -m webbrowser -t http://127.0.0.1:5000
-octoprint serve -b testenv/config
+octoprint serve -b testenv/testconfig
