75e15efdedb8a72fb50a4dfd0f461a9d25a6aa33
Cameron Cross <cameroncros@gmail.com>
2018-08-10 18:41:46 +1000

    Minor code cleanup for #35

--- octoprint_discordremote/command_plugins/enclosure_control.py
+++ octoprint_discordremote/command_plugins/enclosure_control.py
@@ -1,7 +1,6 @@
-import json
 import requests
 
-from octoprint_discordremote.embedbuilder import EmbedBuilder, success_embed, error_embed, info_embed
+from octoprint_discordremote.embedbuilder import EmbedBuilder, success_embed, error_embed
 
 
 class EnclosureControl:
@@ -16,20 +15,23 @@ class EnclosureControl:
             command.command_dict["/outputon"] = {
                 'cmd': self.on,
                 'params': "[ID]",
-                'description': ("Turn on the selected output.\nUses OctoPrint-Enclosure plugin. "
-                                "\nThe ID number can be found in the Enclosure tab in OctoPrint settings. "
-                                "\nThis only supports basic GPIO outputs.")
+                'description': ("Turn on the selected output.\n"
+                                "Uses OctoPrint-Enclosure plugin.\n"
+                                "The ID number can be found in the Enclosure tab in OctoPrint settings.\n"
+                                "This only supports basic GPIO outputs.")
             }
             command.command_dict["/outputoff"] = {
                 'cmd': self.off,
                 'params': "[ID]",
-                'description': ("Turn off the selected output.\nUses OctoPrint-Enclosure plugin. "
-                                "\nThe ID number can be found in the Enclosure tab in OctoPrint settings. "
-                                "\nThis only supports basic GPIO outputs.")
+                'description': ("Turn off the selected output.\n"
+                                "Uses OctoPrint-Enclosure plugin.\n"
+                                "The ID number can be found in the Enclosure tab in OctoPrint settings.\n"
+                                "This only supports basic GPIO outputs.")
             }
             command.command_dict["/outputstatus"] = {
-                'cmd': self.encStatus,
-                'description': "Get the status of all configured outputs.\nUses OctoPrint-Enclosure plugin."
+                'cmd': self.enc_status,
+                'description': "Get the status of all configured outputs.\n"
+                               "Uses OctoPrint-Enclosure plugin."
             }
 
     def on(self, params):
@@ -42,8 +44,9 @@ class EnclosureControl:
         data = result.json()
 
         if data['success']:
-            return None, success_embed(title="Turned ID " + params[1] + " on.")
-        return None, error_embed(title="Failed to turn ID " + params[1] + " on.", description=str(result.content))
+            return None, success_embed(title="Turned ID %i on." % int(params[1]))
+        return None, error_embed(title="Failed to turn ID %i on." % int(params[1]),
+                                 description=str(result.content))
 
     def off(self, params):
         if len(params) > 2:
@@ -54,21 +57,20 @@ class EnclosureControl:
         data = result.json()
 
         if data['success']:
-            return None, success_embed(title="Turned ID " + params[1] + " off.")
-        return None, error_embed(title="Failed to turn ID " + params[1] + " off.", description=str(result.content))
+            return None, success_embed(title="Turned ID %i off." % int(params[1]))
+        return None, error_embed(title="Failed to turn ID %i off." % int(params[1]),
+                                 description=str(result.content))
 
-    def encStatus(self):
+    def enc_status(self):
         result = self.api_command("state", -1)
         data = result.json()
 
         builder = EmbedBuilder()
         builder.set_title('Enclosure Status')
-        title = ""
-        description = ""
 
-        for x in data:
-            title = ("ID: " + str(x['index_id']))
-            description = str(x['status'])
+        for file in data:
+            title = ("ID: %s" % str(file['index_id']))
+            description = str(file['status'])
 
             builder.add_field(title=title, text=description)
 
@@ -77,12 +79,14 @@ class EnclosureControl:
 
     def api_command(self, command, id):
         api_key = self.plugin.get_settings().global_get(["api", "key"])
-        port = self.plugin.get_settings().global_get(["server", "port"])
+        port = int(self.plugin.get_settings().global_get(["server", "port"]))
 
+        id = int(id)
+        req = ""
         if command == "on":
-            req = ("http://127.0.0.1:%s/plugin/enclosure/setIO?status=true&index_id=" % port) + id + "&apikey=" + api_key
+            req = "http://127.0.0.1:%i/plugin/enclosure/setIO?status=true&index_id=%i&apikey=%s" % (port, id, api_key)
         elif command == "off":
-            req = ("http://127.0.0.1:%s/plugin/enclosure/setIO?status=false&index_id=" % port) + id + "&apikey=" + api_key
+            req = "http://127.0.0.1:%i/plugin/enclosure/setIO?status=false&index_id=%i&apikey=%s" % (port, id, api_key)
         elif command == "state":
-            req = ("http://127.0.0.1:%s/plugin/enclosure/getOutputStatus?apikey=" % port) + str(api_key)
+            req = "http://127.0.0.1:%i/plugin/enclosure/getOutputStatus?apikey=%s" % (port, api_key)
         return requests.get(req)
