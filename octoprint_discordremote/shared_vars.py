dad5a9b939b0bdb67c82a09b67667280e22a2280
OctopusProteins <elijahheejoon@live.com>
2018-08-11 09:24:16 -0400

    Fixed merge error

--- octoprint_discordremote/shared_vars.py
+++ octoprint_discordremote/shared_vars.py
@@ -1,11 +1,6 @@
 #Used for accessing variables across classes when imports are not viable
 
-
-<<<<<<< HEAD
 base_url = ""
-=======
-base_url = "HELLO"
->>>>>>> parent of 1e87967... Added support to change command prefixes
 
 
 def init(bu):
