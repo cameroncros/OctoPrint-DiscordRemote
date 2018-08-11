9ccc1397d14561e5c2373b2135d801e0df6f1e37
OctopusProteins <elijahheejoon@live.com>
2018-08-11 09:22:59 -0400

    Revert "Added support to change command prefixes"
    This reverts commit 1e8796749688cfd40124afbbe53581cb47230198.

--- octoprint_discordremote/shared_vars.py
+++ octoprint_discordremote/shared_vars.py
@@ -1,7 +1,11 @@
 #Used for accessing variables across classes when imports are not viable
 
 
+<<<<<<< HEAD
 base_url = ""
+=======
+base_url = "HELLO"
+>>>>>>> parent of 1e87967... Added support to change command prefixes
 
 
 def init(bu):
