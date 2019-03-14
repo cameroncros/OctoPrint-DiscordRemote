39d1e73e2ac00c93698b0c434d54589c4203a345
Cameron Cross <cameroncros@gmail.com>
2019-03-15 09:23:31 +1100

    DR-61 Enabled test.sh to work in windows bash
    # Conflicts:
    #	test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 # Setup testenv on *nix os's
+PYTHON=python2
+ACTIVATE='source testenv/bin/activate'
+which $PYTHON > /dev/null 2>&1
+if [ $? == 1 ]; then
+    PYTHON='py -2'
+    ACTIVATE='source testenv/Scripts/activate'
+fi
+$PYTHON -m virtualenv testenv
+$ACTIVATE
 
-python2 -m virtualenv testenv
-
-source testenv/bin/activate
-pip install https://get.octoprint.org/latest
-python2 setup.py develop
-cp -rf testconfig testenv/
-pip install PyYAML
-python2 configtest.py
-python -m webbrowser -t http://127.0.0.1:5000
+$PYTHON -m pip install --upgrade --no-cache-dir https://get.octoprint.org/latest
+$PYTHON setup.py develop
+$PYTHON configtest.py
+$PYTHON -m webbrowser -t http://127.0.0.1:5000
 FAKE_SNAPSHOT=unittests/test_pattern.png octoprint serve -b testenv/testconfig
