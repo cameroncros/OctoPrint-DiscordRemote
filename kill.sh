300f428ab275e1bc5246dc5c48a0a89a1ddd4846
Cameron Cross <cameroncros@gmail.com>
2019-03-18 15:17:54 +1100

    DR-61 Fixed kill.sh script

--- kill.sh
+++ kill.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-OCTOPRINT_STRING=`ps -e | grep octoprint`
-if [[ $? -ne 0 ]]; then
-    echo Octoprint not running
-    exit 1
-fi
-
 ps -e | grep WINPID 2>&1
-if [[ $? == 0 ]]; then
-    OCTOPRINT_PID=`${OCTOPRINT_STRING} | awk '{print $1;}'`
-    kill -9 $OCTOPRINT_PID
+if [[ $? -ne 0 ]]; then
+    OCTOPRINT_PID=`ps -e | grep octoprint 2>&1 | awk '{print $1;}'`
+    if [[ ${OCTOPRINT_PID} == "" ]]; then
+        echo Octoprint not running
+        exit 1
+    fi
+    kill -9 ${OCTOPRINT_PID}
 else
-    OCTOPRINT_PID=`${OCTOPRINT_STRING} | awk '{print $4;}'`
-    taskkill -PID $OCTOPRINT_PID -F
+    OCTOPRINT_PID=`wmic process get processid, commandline -format:csv | grep octoprint | awk -F ',' '{print $3}'`
+    OCTOPRINT_PID=`echo $OCTOPRINT_PID | awk -F '\r' '{print $1}'`
+    if [[ ${OCTOPRINT_PID} == "" ]]; then
+        echo Octoprint not running
+        exit 1
+    fi
+    taskkill -PID ${OCTOPRINT_PID} -F
 fi
-
-
