500b015a892f5d5d4998cf1eae15e9d7c9e21cf1
Cameron Cross <cameroncros@gmail.com>
2019-07-20 21:30:45 +1000

    Updated release.sh to work slightly better

--- release.sh
+++ release.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+which py > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+    echo "Running in Windows"
+    PYTHON='python'
+    py -2 -m virtualenv testenv
+    source testenv/Scripts/activate
+else
+    echo "Running in Linux"
+    PYTHON=python2
+    python2 -m virtualenv testenv
+    source testenv/bin/activate
+fi
+
+${PYTHON} -m pip install bumpversion
 git checkout dev
 git pull
 git checkout master
