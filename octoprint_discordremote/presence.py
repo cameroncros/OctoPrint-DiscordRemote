1275a4956d4843fa9e11223b7272f968bec2cd79
OctopusProteins <elijahheejoon@live.com>
2020-10-14 20:17:15 -0400

    Update octoprint_discordremote/presence.py
    Co-authored-by: Cameron <cameroncros@gmail.com>

--- octoprint_discordremote/presence.py
+++ octoprint_discordremote/presence.py
@@ -34,7 +34,7 @@ class Presence:
     def presence(self):
         presence_cycle = {
             0: "{}help".format(self.plugin.get_settings().get(["prefix"])),
-            1: "{}".format(self.generate_status())
+            1: self.generate_status()
         }
         while not self.discord.shutdown_event.is_set():                
 
