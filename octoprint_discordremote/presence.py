ceabe1c844adb14cf144bffae41b1ca24c725ca3
Cameron <cameroncros@gmail.com>
2022-08-26 21:15:58 +1000

    Revert "Fixed presence"

--- octoprint_discordremote/presence.py
+++ octoprint_discordremote/presence.py
@@ -19,7 +19,6 @@ class Presence:
         self.discord: Optional['DiscordImpl'] = None
         self.presence_cycle_id: int = 0
         self.presence_thread: Optional[Thread] = None
-        self.last_status = None
 
     def configure_presence(self, plugin: 'DiscordRemotePlugin', discord: 'DiscordImpl'):
         self.plugin = plugin
@@ -31,31 +30,33 @@ class Presence:
             self.presence_thread = Thread(target=self.presence)
             self.presence_thread.start()
 
-    def update(self, status, msg):
-        if not status == self.last_status:
-            self.discord.update_presence(status, msg)
-            self.last_status = status
-
     def generate_status(self):
         if self.plugin.get_printer().is_operational():
             if self.plugin.get_printer().is_printing():
                 job_name = self.plugin.get_printer().get_current_data()['job']['file']['name']
                 job_percent = self.plugin.get_printer().get_current_data()['progress']['completion']
-                return ["online", "{}% of {} complete".format(
-                                                  humanfriendly.format_number(job_percent, num_decimals=2),
-                                                  job_name)]
+                return "Printing {} - {}%".format(job_name,
+                                                  humanfriendly.format_number(job_percent, num_decimals=2))
             else:
-                return ["idle", " and waiting"]
+                return "Idle."
         else:
-            return ["offline", "for printer to come online"]
+            return "Not operational."
 
     def presence(self):
+        presence_cycle = {
+            0: "{}help".format(self.plugin.get_settings().get(["prefix"])),
+            1: self.generate_status()
+        }
         while not self.discord.shutdown_event.is_set():
             if self.plugin.get_settings().get(['presence']):
-                [status, presence] = self.generate_status()
-                self.update(status, presence)
+                presence_cycle[1] = "{}".format(self.generate_status())
+                self.discord.update_presence(presence_cycle[self.presence_cycle_id % len(presence_cycle)])
+
+                self.presence_cycle_id += 1
+                if self.presence_cycle_id == len(presence_cycle):
+                    self.presence_cycle_id = 0
             else:
-                self.update("online", None)
+                self.discord.update_presence(None)
 
             for i in range(int(self.plugin.get_settings().get(['presence_cycle_time']))):
                 if not self.discord.shutdown_event.is_set():
