6da4b3c5cac34711099f3cabc350331ab22d328d
Alfredo Colas Gullon <57442104+LyricPants66133@users.noreply.github.com>
2023-04-22 16:05:15 -0400

    Fixed Python Compatibility

--- octoprint_discordremote/__init__.py
+++ octoprint_discordremote/__init__.py
@@ -667,7 +667,7 @@ class DiscordRemotePlugin(octoprint.plugin.EventHandlerPlugin,
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
 __plugin_name__ = "DiscordRemote"
-__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_pythoncompat__ = ">=3.8,<4"
 
 
 def __plugin_load__():
