ed558f15625e02b0c278a4f27ac14b615bd1839a
Cameron Cross <cameroncros@gmail.com>
2024-11-25 11:35:58 +1100

    Bump version for next release

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ plugin_package = "octoprint_discordremote"
 plugin_name = "OctoPrint-DiscordRemote"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "5.13.0"
+plugin_version = "5.14.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
