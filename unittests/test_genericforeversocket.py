132f7feea79caa3783ae161aebaf76f698ea157b
Cameron Cross <cameroncros@gmail.com>
2026-05-10 18:25:31 +1000

    Fix Tuple is not callable error.

--- unittests/test_genericforeversocket.py
+++ unittests/test_genericforeversocket.py
@@ -1,23 +1,30 @@
 import contextlib
+import random
 import time
 from socket import socket
 from unittest import TestCase
 
 from octoprint_discordremote.genericforeversocket import GenericForeverSocket
 
+null_init = lambda x: None
+null_read = lambda x: None
+null_write = lambda x, y: None
 
 class TestDiscordLink(TestCase):
 
+    def rand_port(self):
+        return random.randint(10000, 65534)
+
     def test_start_stop_rapid(self):
-        gfs = GenericForeverSocket(address='127.0.0.1', port=34567, read_fn=(), write_fn=(),
-                                   init_fn=())
+        gfs = GenericForeverSocket(address='127.0.0.1', port=self.rand_port(), read_fn=null_read, write_fn=null_write,
+                                   init_fn=null_init)
         gfs.run()
         with self.timer(3):
             gfs.stop()
 
     def test_basic_never_connected(self):
-        gfs = GenericForeverSocket(address='127.0.0.1', port=34567, read_fn=(), write_fn=(),
-                                   init_fn=())
+        gfs = GenericForeverSocket(address='127.0.0.1', port=self.rand_port(), read_fn=null_read, write_fn=null_write,
+                                   init_fn=null_init)
         gfs.run()
 
         for i in range(3):
@@ -30,12 +37,13 @@ class TestDiscordLink(TestCase):
         self.assertEqual(3, len(gfs.queued_messages))
 
     def test_non_blocking(self):
-        gfs = GenericForeverSocket(address='127.0.0.1', port=34567, read_fn=(), write_fn=(),
-                                   init_fn=())
+        port = self.rand_port()
+        gfs = GenericForeverSocket(address='127.0.0.1', port=port, read_fn=null_read, write_fn=null_write,
+                                   init_fn=null_init)
         gfs.run()
 
         s = socket()
-        s.bind(('127.0.0.1', 34567))
+        s.bind(('127.0.0.1', port))
         s.listen()
         client, addr = s.accept()
         # gfs connected, and we have accepted the connection.
@@ -48,13 +56,13 @@ class TestDiscordLink(TestCase):
     def test_partial_recv(self):
         def read_fn(s: GenericForeverSocket.BufferedSocketWrapper):
             data = s.peek(4)
-
-        gfs = GenericForeverSocket(address='127.0.0.1', port=34567, read_fn=read_fn, write_fn=(),
-                                   init_fn=())
+        port = self.rand_port()
+        gfs = GenericForeverSocket(address='127.0.0.1', port=port, read_fn=read_fn, write_fn=null_write,
+                                   init_fn=null_init)
         gfs.run()
 
         s = socket()
-        s.bind(('127.0.0.1', 34567))
+        s.bind(('127.0.0.1', port))
         s.listen()
         client, addr = s.accept()
         client.send(b'123')  # Expecting 4 bytes, but only sent 4
